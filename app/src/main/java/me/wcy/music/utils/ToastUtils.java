a838450a91c46824110bc0c002002055b5f8c192
hzwangchenyan <hzwangchenyan@corp.netease.com>
2016-09-26 17:50:03 +0800

    优化Toast

--- app/src/main/java/me/wcy/music/utils/ToastUtils.java
+++ app/src/main/java/me/wcy/music/utils/ToastUtils.java
@@ -9,16 +9,22 @@ import android.widget.Toast;
  */
 public class ToastUtils {
     private static Context sContext;
+    private static Toast sToast;
 
     public static void init(Context context) {
         sContext = context.getApplicationContext();
     }
 
     public static void show(int resId) {
-        Toast.makeText(sContext, resId, Toast.LENGTH_SHORT).show();
+        show(sContext.getString(resId));
     }
 
     public static void show(String text) {
-        Toast.makeText(sContext, text, Toast.LENGTH_SHORT).show();
+        if (sToast == null) {
+            sToast = Toast.makeText(sContext, text, Toast.LENGTH_SHORT);
+        } else {
+            sToast.setText(text);
+        }
+        sToast.show();
     }
 }
