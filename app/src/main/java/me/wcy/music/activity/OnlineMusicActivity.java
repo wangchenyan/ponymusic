d3e7890ce2bea6b5a59b9a3d51e0d0f6edccb4c6
Inzmam ul haq khan <inzkhan@gmail.com>
2017-12-22 01:08:04 +0500

    fixed IllegalArgumentException error

--- app/src/main/java/me/wcy/music/activity/OnlineMusicActivity.java
+++ app/src/main/java/me/wcy/music/activity/OnlineMusicActivity.java
@@ -14,7 +14,6 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
